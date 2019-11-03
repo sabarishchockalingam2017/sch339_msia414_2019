6b040186ef639e61ce9947afad94bf34d830b6bc
sabarishchockalingam2017 <sabarishchockalingam2017@u.northwestern.edu>
2019-11-03 00:25:45 -0500

    completed summary stats code

---
+++ dataset_stats.py
@@ -0,0 +1,49 @@
+import os
+import json
+import re
+import pandas as pd
+import nltk
+import numpy as np
+
+# loading corpus (all text files) into string variable
+datapath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
+
+# getting names of json files
+jsonfiles = [file for file in os.listdir(datapath) if file.endswith('.json')]
+jsonpatt = re.compile(r'^(.*?).json')
+
+# loading json files into a dictionary
+docdict = {}
+for doc in jsonfiles:
+    print("loading {}".format(doc))
+    templist = []
+    for line in open(os.path.join(datapath, doc), mode='r', encoding='utf8'):
+        templist.append(json.loads(line))
+    docname = jsonpatt.findall(doc)[0]
+    docdict[docname] = templist
+    print("appended {} list to dict".format(docname))
+
+# creating dataframes to get summary statistics
+dfdict = {}
+
+for doc in jsonfiles:
+    docname = jsonpatt.findall(doc)[0]
+    tempdf = pd.DataFrame(data = docdict[docname])
+    dfdict[docname] = tempdf
+    print('created {} df'.format(docname))
+
+# getting some preliminary stats for reviews data
+print('Basic stats for reviews data: ')
+print(dfdict['review'].describe(include='all'))
+
+# taking only first 500k since entire dataset is too large (6mil+)
+allreviewsstring = ' '.join(dfdict['review']['text'].head(500000))
+# tokenizing
+tokens = nltk.tokenize.word_tokenize(allreviewsstring)
+# normalizing
+# filter out non-alphabetic
+words = [w for w in tokens if w.isalpha()]
+
+avg_wordlen = np.mean([len(word) for word in words])
+
+print("The average word length in yelp reviews is {} letters.".format(avg_wordlen))
